9b1c283a242f3e480d313e6edaa12e56ef477952
Binarma <Binamrakc2015@gmail.com>
2026-07-15 21:56:58 +0545

    Property Database added

--- backend/Intializer/syncdatabase.go
+++ backend/Intializer/syncdatabase.go
@@ -1,6 +1,8 @@
 package intializer
 
-import "log"
+import (
+	"log"
+)
 
 func User() {
 
@@ -22,6 +24,31 @@ func User() {
 		log.Fatal("failed to exec the register table")
 	} else {
 		log.Println("register table created")
+	}
+
+	Listing := `
+	create table if not exists Property(
+	id int AUTO_INCREMENT primary key,
+	Title varchar(100) not null,
+	description text,
+	property_type Enum('apartment','house','villa','office')Not null,
+	price int NOT NULL,
+	listing_type Enum('sale','rent')Not null,
+	Property_Status Enum('available','Booked','Sold'),
+	Status Enum('pending','approved','rejected') Default 'pending',
+	address VARCHAR(255) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+	Created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+	INDEX idx_price(price),
+	INDEX idx_prop_add(address,city),
+	INDEX idx_prop_type(property_type)
+	);`
+	_, err = DB.Exec(Listing)
+	if err != nil {
+		log.Fatal("failed to exec db listing")
+	} else {
+		log.Println("Listing table created")
 	}
 	log.Printf("table execute sucessfully")
 }
