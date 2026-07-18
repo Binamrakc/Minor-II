f49b4960d81f46aff3b5638ad43ad2925912bffb
Binarma <Binamrakc2015@gmail.com>
2026-07-18 18:41:58 +0545

    contact us func created

--- backend/controller/contact.go
+++ backend/controller/contact.go
@@ -1,11 +1,15 @@
 package controller
 
-import "net/http"
+import (
+	"mis/model"
+	"net/http"
+)
 
 func Contact(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Unauthorized Method", http.StatusUnauthorized)
 		return
 	}
+	var contact model.Contact
 
 }
